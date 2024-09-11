6dc0f5eeba14982d850d0f05372186f67fffb25e
Mário Guimarães <mariorochedo329@gmail.com>
2024-09-11 15:57:00 -0300

    Create codigo.js

---
+++ codigo.js
@@ -0,0 +1,10 @@
+const num1 = 10.50;
+const num2 = 10.36;
+const soma = num1 + num2;
+const sub = num1 - num2
+const mult = num1 * num2
+const div = num1 / num2
+console.log(`${num1} + ${num2} = ${soma}\n 
+${num1} X ${num2} = ${mult}\n
+${num1} - ${num2} = ${sub.toFixed(2)}\n
+${num1} / ${num2} = ${div.toFixed(2)}`);
